3d6b790e4f16fadae8ea69ea5ec9567f4435beeb
Joy <joydebmondal28@gmail.com>
2018-08-16 12:28:24 +0530

    prediction addded

--- Named_Entity-Recognition_Bidirectional_LSTM_CNN/predict.py
+++ Named_Entity-Recognition_Bidirectional_LSTM_CNN/predict.py
@@ -1,46 +1,84 @@
-from prepro import createBatches,createMatrices,addCharInformatioin,padding
-from nn import word2Idx, label2Idx, case2Idx, char2Idx, idx2Label
+from preprocessing import createBatches,createMatrices,addCharInformatioin,padding
+from keras.models import load_model
+import pickle
+
+
+print("Model is loading...")
+with open("Benchmark_Models/dict/wd_to_id.txt", "rb") as myFile:
+    wd_to_id = pickle.load(myFile)
+
+with open("Benchmark_Models/dict/lb_to_id.txt", "rb") as myFile:
+    lb_to_id = pickle.load(myFile)
+
+with open("Benchmark_Models/dict/ch_to_id.txt", "rb") as myFile:
+    ch_to_id = pickle.load(myFile)
+
+with open("Benchmark_Models/dict/idx2Label.txt", "rb") as myFile:
+    idx2Label = pickle.load(myFile)
+
+
+
+
 #from nn import entity_extract_model_BLSTM
 import numpy as np
 
-entity_extract_model_BLSTM = None
+entity_model = load_model('Benchmark_Models/Benchmark_all_Entity_Model_try_with_book_flight.h5')
 
-def getModel():
-    global entity_extract_model_BLSTM
-    if None==entity_extract_model_BLSTM:
-        from nn import entity_extract_model_BLSTM
+print("Completed")
+
 
 def predict(sentence,model):
     sen_list = [[[i,'O\n'] for i in sentence.split()]]
-    #sen_list = [[['SOCCER', 'O\n'], ['-', 'O\n'], ['JAPAN', 'O\n'], ['GET', 'O\n'], ['LUCKY', 'O\n'], ['WIN', 'O\n'], [',', 'O\n'], ['CHINA', 'O\n'], ['IN', 'O\n'], ['SURPRISE', 'O\n'], ['DEFEAT', 'O\n'], ['.', 'O\n']]]
     test = addCharInformatioin(sen_list)
     
     predLabels = []
     
-    test_set = padding(createMatrices(test, word2Idx, label2Idx, case2Idx,char2Idx))
+    test_set = padding(createMatrices(test,wd_to_id,  lb_to_id, ch_to_id))
     
     test_batch,test_batch_len = createBatches(test_set)
-    
-    for i,data in enumerate(test_batch):    
-        tokens, casing,char, labels = data
-        #print(tokens, casing,char, labels)
+    for i,data in enumerate(test_batch):
+        tokens, char, labels = data
         tokens = np.asarray([tokens])     
-        casing = np.asarray([casing])
         char = np.asarray([char])
-        pred = model.predict([tokens, casing,char], verbose=False)[0] 
+        pred = model.predict([tokens,char], verbose=False)[0] 
         pred = pred.argmax(axis=-1) #Predict the classes            
         predLabels.append(pred)
     entity_labels = []
     j = 0
     words_list = sentence.split()
     for i in predLabels[-1]:
-        entity_labels.append((words_list[j],idx2Label[int(i)]))
+        entity_labels.append((words_list[j],idx2Label[int(i)].replace("\n","")))
         j+=1
-    print("predLabels",entity_labels)    
     
     return entity_labels
 
-getModel()
-sentence ="a flight from BLR to MAA on 2018/07/30"
-sentence ="SOCCER - JAPAN GET LUCKY WIN CHINA IN SURPRISE DEFEAT."
-predict(sentence,entity_extract_model_BLSTM)
+#sentence ="a flight from ccu to hyd on 2018/07/30"
+#list_sen = ["a flight from ccu to hyd on 2018/07/30","Make a booking from ccu to hyd on 2018/07/30","CCU","hyd"]
+test_File = open("Test_Data/Test_benchmark.txt")
+test_list = test_File.readlines()
+
+test_list = ['book a flight for Goa to haryana on 2016/11/30']
+pred_benchmark_list = []
+for i in test_list:
+    pred_benchmark_list.append(predict(i,entity_model))
+
+print("Enter/Paste your content. Press double enter to exit.")
+
+contents = []
+while True:
+    try:
+        line = input()
+        entity_prediction = predict(line,entity_model)
+        for i in entity_prediction:
+            print(i)
+    except EOFError:
+        break
+    contents.append(line)
+'''    
+def make_txt(list):
+    
+    f = open("Test_Data/benchmark_entity_result.txt",'w')  
+    f.write("\n".join(list))
+    
+make_txt(pred_benchmark_list)
+'''
